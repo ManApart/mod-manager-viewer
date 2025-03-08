6f1fb92829639a94bbc7c709a0bc0d0d391833ce
ManApart <r@austinkucera.com>
2025-03-08 08:46:25 -0500

    table view mod stats

--- src/jsMain/kotlin/pages/ModView.kt
+++ src/jsMain/kotlin/pages/ModView.kt
@@ -2,13 +2,16 @@ package org.manapart.pages
 
 import kotlinx.dom.addClass
 import kotlinx.dom.removeClass
+import kotlinx.html.TagConsumer
 import kotlinx.html.id
 import kotlinx.html.js.*
 import kotlinx.html.span
+import kotlinx.html.table
 import org.manapart.el
 import org.manapart.getMods
 import org.manapart.replaceElement
 import org.manapart.updateUrl
+import org.w3c.dom.HTMLElement
 
 fun modView() {
     updateUrl("mods")
@@ -16,21 +19,15 @@ fun modView() {
         div {
             id = "mods"
             h1 { +"Mod Viewer" }
-            p { +"Upload your data.json to view your mod list on the go!" }
-            button {
-                +"Upload"
-                onClickFunction = { uploadView() }
+            p {
+                button {
+                    id = "upload-button"
+                    +"Upload"
+                    onClickFunction = { uploadView() }
+                }
+                +"your data.json to view your mod list on the go!"
             }
-            div {
-                id = "header"
-                span("idStat") { +"Id" }
-                span("stat versionStat") { +"Version" }
-                span("stat loadStat") { +"Load" }
-                span("stat enabledStat") { +"Enabled" }
-                span("stat endorsedStat") { +"Endorsed" }
-                span("stat categoryStat") { +"Category" }
-                br {  }
-            }
+            hr {  }
             getMods().forEach { mod ->
                 div("modRow") {
                     div("nameRow") {
@@ -43,20 +40,29 @@ fun modView() {
                     }
                     div("statsRow minimized") {
                         id = "${mod.id ?: mod.name}-stats-row"
-                        span("idStat") { +(mod.id?.toString() ?: "?") }
-                        //TODO - show update icon
-                        span("stat versionStat") { +(mod.version ?: "?") }
-                        span("stat loadStat") { +mod.loadOrder.toString() }
-                        //Show icons
-                        span("stat enabledStat") { +mod.enabled.toString() }
-                        span("stat endorsedStat") { +(mod.endorsed?.toString() ?: "") }
-                        span("stat categoryStat") { +(mod.categoryId?.toString() ?: "") }
-                        val tagContent = if (mod.tags.isNotEmpty()) mod.tags.joinToString() else ""
-                        span("stat tagsStat") { +tagContent }
+
+                        table("statsRowTable") {
+                            tableRow("Id", mod.id?.toString() ?: "?")
+                            tableRow("Version", mod.version ?: "?")
+                            tableRow("Load Order", mod.loadOrder.toString())
+                            tableRow("Enabled", mod.enabled.toString())
+                            tableRow("Endorsed", mod.endorsed?.toString() ?: "")
+                            tableRow("Category", mod.categoryId?.toString() ?: "")
+                            val tagContent = if (mod.tags.isNotEmpty()) mod.tags.joinToString() else ""
+                            tableRow("Tags", tagContent)
+                        }
+//                        //TODO - show update icon
                     }
                 }
             }
         }
+    }
+}
+
+fun TagConsumer<HTMLElement>.tableRow(header: String, value: String) {
+    tr {
+        td { +header }
+        td { +value }
     }
 }
 
